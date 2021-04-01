207a01ff1470394db330d852ae4b38df85b131db
yasmeenokh <ykhatib44@gmail.com>
2021-04-01 20:29:11 +0300

    first feature done

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # portfolio-Yasmeen
+
+### Number and name of feature: Feature #1: Deployment
+
+Estimate of time needed to complete: an hour 
+
+Start time: 7
+
+Finish time: 7:30
+
+Actual time needed to complete: 30 min. 
+
+
+### Number and name of feature: Feature #2: Feature #2: Refactor the CSS
+
+Estimate of time needed to complete: an hour 
+
+Start time: 7
+
+Finish time: 7:30
+
+Actual time needed to complete: 30 min. 
+
+### Number and name of feature: Feature #3: Modify the contents
+
+Estimate of time needed to complete: an hour 
+
+Start time: 7
+
+Finish time: 7:30
+
+Actual time needed to complete: 30 min. 
---
+++ server.js
@@ -0,0 +1,32 @@
+'use strict';
+
+// To run the server we use npm start
+// Whenever we change something in the js we should ctrl+c
+
+
+// To use the express library
+const express = require('express');
+
+// To be able to use the express properties 
+const server = express();
+const PORT = process.env.PORT || 3000;
+// to access the files in the public folders
+server.use(express.static('./public'));
+
+// Our Routs 
+
+// To test that the everything is working, the console will appear in the terminal
+// localhost:3000/test 
+server.get('/test', (request, response)=>{
+    response.send('The server is working')
+}); 
+// localhost:3000/data
+server.get('/data', (request, response)=>{
+    let info =[{myName: 'Yasmeen'}, {myFamily: 'Al-Khateeb'}]
+    response.json(info);
+    // we can also use .send as well
+})
+// To make the server start listening
+server.listen(PORT, ()=>{
+    console.log(`listening on port ${PORT}`)
+}); 
