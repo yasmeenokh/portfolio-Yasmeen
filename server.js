bfed20edba38777ce190101be62d36220c7a9eb8
yasmeenokh <ykhatib44@gmail.com>
2021-04-04 12:43:38 +0300

    added the eslint file

--- server.js
+++ server.js
@@ -5,28 +5,28 @@
 
 
 // To use the express library
-const express = require('express');
+const express = require( 'express' );
 
-// To be able to use the express properties 
+// To be able to use the express properties
 const server = express();
 const PORT = process.env.PORT || 3000;
 // to access the files in the public folders
-server.use(express.static('./public'));
+server.use( express.static( './public' ) );
 
-// Our Routs 
+// Our Routs
 
 // To test that the everything is working, the console will appear in the terminal
-// localhost:3000/test 
-server.get('/test', (request, response)=>{
-    response.send('The server is working')
-}); 
+// localhost:3000/test
+server.get( '/test', ( request, response )=>{
+  response.send( 'The server is working' );
+} );
 // localhost:3000/data
-server.get('/data', (request, response)=>{
-    let info =[{myName: 'Yasmeen'}, {myFamily: 'Al-Khateeb'}]
-    response.json(info);
-    // we can also use .send as well
-})
+server.get( '/data', ( request, response )=>{
+  let info = [{myName: 'Yasmeen'}, {myFamily: 'Al-Khateeb'}];
+  response.json( info );
+  // we can also use .send as well
+} );
 // To make the server start listening
-server.listen(PORT, ()=>{
-    console.log(`listening on port ${PORT}`)
-}); 
+server.listen( PORT, ()=>{
+  console.log( `listening on port ${PORT}` );
+} );
